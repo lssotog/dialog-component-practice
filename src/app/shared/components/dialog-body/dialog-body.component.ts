4e0d6e704c836da98d290d80b4034b3a33447494
sotol067 <luz.soto.-nd@disney.com>
2024-05-30 10:51:08 -0500

    Created components

---
+++ src/app/shared/components/dialog-body/dialog-body.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'custom-dialog-body',
+  standalone: true,
+  imports: [],
+  templateUrl: './dialog-body.component.html',
+  styleUrl: './dialog-body.component.scss'
+})
+export class DialogBodyComponent {
+
+}
