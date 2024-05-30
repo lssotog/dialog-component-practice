acec7cd4002daf1b1a42fd9e0727a1310b5cd8bb
sotol067 <luz.soto.-nd@disney.com>
2024-05-30 11:47:47 -0500

    Layout input signal added

--- src/app/shared/components/dialog/dialog.component.ts
+++ src/app/shared/components/dialog/dialog.component.ts
@@ -1,14 +1,18 @@
-import { Component, ElementRef, OnInit, viewChild } from '@angular/core';
+import { NgTemplateOutlet } from '@angular/common';
+import { Component, ElementRef, OnInit, input, viewChild } from '@angular/core';
 @Component({
   selector: 'custom-dialog',
   standalone: true,
-  imports: [],
+  imports: [
+    NgTemplateOutlet
+  ],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.scss'
 })
 export class DialogComponent implements OnInit {
   dialog = viewChild<ElementRef<HTMLDialogElement>>('dialogRef')
-
+  layout = input<string>('default')
+ 
   ngOnInit(): void {
     
   }
