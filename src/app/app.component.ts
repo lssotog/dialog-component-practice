cbcb4f8cf8e6af76e98a4547a9544328f4e6d72d
sotol067 <luz.soto.-nd@disney.com>
2024-06-19 11:49:06 -0500

    showModal event added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ModalComponent } from './shared/components/modal/modal.component';
+import { DialogComponent } from './shared/components/dialog/dialog.component';
+import { Position } from './shared/types/positionsEnum';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    ModalComponent
+    ModalComponent,
+    DialogComponent, 
+    CommonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'dialog-component-practice';
+  position = Position;
+  label = '';
 }
--- src/app/shared/components/dialog/dialog.component.ts
+++ src/app/shared/components/dialog/dialog.component.ts
@@ -1,11 +1,12 @@
-import { NgTemplateOutlet } from '@angular/common';
+import { CommonModule, NgTemplateOutlet } from '@angular/common';
 import { Component, ElementRef, OnInit, input, viewChild } from '@angular/core';
 import { Position } from '../../types/positionsEnum';
 @Component({
   selector: 'custom-dialog',
   standalone: true,
   imports: [
-    NgTemplateOutlet
+    NgTemplateOutlet,
+    CommonModule
   ],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.scss'
@@ -17,5 +18,9 @@ export class DialogComponent implements OnInit {
  
   ngOnInit(): void {
     
+    if(!this.dialog()?.nativeElement.open){
+      this.dialog()?.nativeElement.showModal()
+    }
   }
+
 }
